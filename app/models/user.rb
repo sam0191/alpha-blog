dff2b31f8e652cea867c641e0bc0b5f220737935
Sam Abu <smer.job@gmail.com>
2016-06-15 08:49:06 -0500

    add association between user and articles and add debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :articles
+	#before the user hit the database it will take the email value and  will turn it to lower case by using downcase method and then saving to the database
+	before_save { self.email = email.downcase }
 	validates :username, presence: true, 
 			  uniqueness: { case_sensitive: false },
 			  length: { minimum: 3, maximum: 25 }
@@ -6,5 +9,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105},
 		  	uniqueness: { case_sensitive: false },
 		  	format: { with: VALID_EMAIL_REGEX }
+
 	
 end
