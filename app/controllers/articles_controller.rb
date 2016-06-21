a72a471a43699dbca241b9877fbc45ea0cd622c0
sam0191 <samer.abunuwara@gmail.com>
2016-06-22 01:29:50 +0300

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
-
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 	def new
 		@article = Article.new
 	end
@@ -44,6 +45,12 @@ class ArticlesController < ApplicationController
 	private
 		def set_article
 			@article = Article.find(params[:id])
+		end
+		def require_same_user
+			unless current_user == @article.user
+				flash[:danger] = "You can only edit or delete your own articles"
+				redirect_to root_path
+			end
 		end
 		def article_params
 			params.require(:article).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, execpt: [:new]
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  # The priority, is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
