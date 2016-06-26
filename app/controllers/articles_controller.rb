822838ce5080900c92ff3b9bf9b52de255a14c3d
sam0191 <samer.abunuwara@gmail.com>
2016-06-26 21:20:10 +0300

    Update all vuews with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
 			end
 		end
 		def article_params
-			params.require(:article).permit(:title, :description)
+			params.require(:article).permit(:title, :description, category_ids: [])
 		end
 
 end
